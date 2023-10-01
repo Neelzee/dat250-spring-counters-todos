20aea3f4c06583e9b04e660b9bd043c00616ecea
Neelzee <nilsien2001@gmail.com>
2023-10-01 19:14:26 +0200

    Dont know why the message stuff isn't working, but

--- src/main/java/no/hvl/dat250/rest/todos/TodoController.java
+++ src/main/java/no/hvl/dat250/rest/todos/TodoController.java
@@ -1,9 +1,95 @@
 package no.hvl.dat250.rest.todos;
 
+import org.springframework.http.StreamingHttpOutputMessage;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
  * Rest-Endpoint for todos.
  */
+@RestController
 public class TodoController {
 
+  private static List<Todo> todos = new ArrayList<>();
+
+  private static long id = 0;
+
   public static final String TODO_WITH_THE_ID_X_NOT_FOUND = "Todo with the id %s not found!";
+
+  @GetMapping("/todos")
+  public List<Todo> getTodos() {
+    return TodoController.todos;
+  }
+
+  @GetMapping("/todos/{id}")
+  public Todo getTodo(@PathVariable long id) {
+    for (Todo t : TodoController.todos) {
+      if (t.getId() == id) {
+        return t;
+      }
+    }
+
+    return errorTodo(id);
+  }
+
+  @PutMapping("/todos/{id}")
+  public Todo putTodo(@RequestBody Todo newTodo, @PathVariable long id) {
+    Todo oldTodo;
+    for (Todo t : TodoController.todos) {
+      if (t.getId() == id) {
+        oldTodo = t;
+
+        oldTodo.setDescription(newTodo.getDescription());
+        oldTodo.setSummary(newTodo.getSummary());
+
+        return oldTodo;
+      }
+    }
+
+    return errorTodo(id);
+  }
+
+  @PostMapping("/todos")
+  public Todo postTodo(@RequestBody Todo todo) {
+    todo.setId(setId());
+
+    todos.add(todo);
+
+    return todo;
+  }
+
+
+  @DeleteMapping("/todos/{id}")
+  public void deleteTodo(@PathVariable long id) {
+    TodoController.todos = TodoController.todos
+            .stream()
+            .filter(
+                    t -> t.getId() != id
+            )
+            .collect(Collectors.toList());
+  }
+
+  /**
+   * Returns the new id, and increments the id counter
+   * @return new id
+   */
+  private long setId() {
+    long id = TodoController.id;
+
+    TodoController.id += 1;
+
+    return id;
+  }
+
+
+  private Todo errorTodo(long id) {
+    Todo err = new Todo();
+    err.setDescription(String.format(TODO_WITH_THE_ID_X_NOT_FOUND, id));
+
+    return err;
+  }
 }
